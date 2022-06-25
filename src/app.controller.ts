00f16f16fcfe76a7b6ccb617ef71f813d715c907
Gracechung-sw <hjngy0511@gmail.com>
2022-06-26 00:38:43 +0900

    WIP: Add ConfigModule that exposes ConfigService to handle config info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService, private configService: ConfigService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		console.log(this.configService.get('TEST_VALUE'));
+		console.log(this.configService.get('NODE_ENV'));
+		return this.appService.getHello();
+	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: [`.env.${process.env.STAGE}`],
+			isGlobal: true,
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
