03a380b536203988d3851f180724714460dacbbe
Gracechung-sw <hjngy0511@gmail.com>
2022-06-26 11:50:32 +0900

    WIP: add sample code how to use configService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService, private configService: ConfigService) {}
+	constructor(private readonly appService: AppService) {}
 
 	@Get()
 	getHello(): string {
-		console.log(this.configService.get('TEST_VALUE'));
-		console.log(this.configService.get('NODE_ENV'));
 		return this.appService.getHello();
 	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			envFilePath: [`.env.${process.env.STAGE}`],
-			isGlobal: true,
-		}),
-	],
+	imports: [ConfigModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
