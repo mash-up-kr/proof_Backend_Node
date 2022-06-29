bf7b4ad4e003fdb88fcddbb415db686b74b460ef
Gracechung-sw <hjngy0511@gmail.com>
2022-06-29 23:26:52 +0900

    WIP: Add user module for sample

---
+++ src/users/user.entity.ts
@@ -0,0 +1,24 @@
+// This is just sample user entity.
+// TODO: Fix to proper user entity when ERD is fixed.
+
+import { Exclude } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { CommonEntity } from 'src/common/entities/common.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class User extends CommonEntity {
+	@IsEmail()
+	@IsNotEmpty()
+	@Column({ type: 'varchar', unique: true, nullable: false })
+	email: string;
+
+	@IsString()
+	@IsNotEmpty()
+	@Column({ type: 'varchar', nullable: false })
+	name: string;
+
+	@Exclude()
+	@Column({ type: 'varchar', nullable: false })
+	password: string;
+}
