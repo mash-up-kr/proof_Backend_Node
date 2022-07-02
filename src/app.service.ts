a77aa881a9fd4ea66dcb89e0b43b83516e7f1110
Gracechung-sw <hjngy0511@gmail.com>
2022-07-02 16:07:47 +0900

    Fix: Delete unused sample code

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,11 +4,7 @@ import { AppConfig, DatabaseConfig } from './config/config.constant';
 
 @Injectable()
 export class AppService {
-	constructor(private readonly configService: ConfigService) {}
-	appConfig = this.configService.get<AppConfig>('appConfig');
-	dbConfig = this.configService.get<DatabaseConfig>('databaseConfig');
-
 	getHello(): string {
-		return `Hello ZuZu! node env: ${this.appConfig.env} , db_name: ${this.dbConfig.dbname}`;
+		return 'Hello ZuZu!';
 	}
 }
