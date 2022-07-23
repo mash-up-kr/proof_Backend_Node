3acac3e988a0c7081c6f03ba752ae5401b7525c8
marades <jkl9510357@gmail.com>
2022-07-23 22:50:31 +0900

    Feat: 월드컵 전체 목록 조회

--- apps/api-server/src/worldcup/worldcup.controller.ts
+++ apps/api-server/src/worldcup/worldcup.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { WolrdCupService } from './worldcup.service';
 
-@Controller()
+@Controller('worldcups')
 export class WorldcupController {
+	constructor(private readonly worldcupService: WolrdCupService) {}
 
+	@Get()
+	async getWorldcups() {
+		const worldcups = await this.worldcupService.getWorldcups();
+		return worldcups;
+	}
 }
--- apps/api-server/src/worldcup/worldcup.service.ts
+++ apps/api-server/src/worldcup/worldcup.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Worldcup } from './worldcup.entity';
 
 @Injectable()
-export class WolrdCupService {}
+export class WolrdCupService {
+	constructor(@InjectRepository(Worldcup) private readonly worldcupRepository: Repository<Worldcup>) {}
+
+	async getWorldcups(): Promise<Worldcup[]> {
+		const worldcups = await this.worldcupRepository.find();
+		return worldcups;
+	}
+}
