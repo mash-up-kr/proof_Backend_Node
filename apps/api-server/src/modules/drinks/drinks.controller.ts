1ba90ac186a84abca4aec4d218bd9602067b6e29
Gracechung-sw <hjngy0511@gmail.com>
2022-08-12 03:23:42 +0900

    Feat: Add find reviewed drinks info by user id

--- apps/api-server/src/modules/drinks/drinks.controller.ts
+++ apps/api-server/src/modules/drinks/drinks.controller.ts
@@ -1,41 +1,43 @@
-import { Controller, Get, Post, Body, Param, BadRequestException, Query } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, BadRequestException, Query, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+import { AuthUser } from '@src/decorators/auth.decorator';
 import { Category } from '@src/types/drinks-category.types';
+import { JwtAuthGuard } from '@src/modules/auth/guards/jwt-auth.guard';
+import { ApiDocs } from './drinks.docs';
 import { DrinksService } from './drinks.service';
-import { CreateDrinkDto } from './dto/create-drink.dto';
+import { User } from '@src/entities/users.entity';
 
 @ApiTags('drinks - 술 상세 정보')
 @Controller('drinks')
 export class DrinksController {
 	constructor(private readonly drinksService: DrinksService) {}
 
-	// TODO: Check if the drink already exists
-	// XXX: Discuss with the team how to check the drink already exists
-	@Post()
-	@ApiOperation({
-		summary: 'Add new drink data',
-		description: 'This endpoint will used by admin(dev team) to adds new drink data to the database.',
-	})
-	createDrinkInfo(@Body() createDrinkDto: CreateDrinkDto) {
-		return this.drinksService.create(createDrinkDto);
-	}
-
 	@Get()
-	public async findAll() {
-		return await this.drinksService.findAll();
+	@ApiDocs.findAllDrinks('전체 술 상세 정보 조회')
+	public async findAllDrinks() {
+		return await this.drinksService.findAllDrinks();
 	}
 
 	@Get('/category')
-	public async findByCategory(@Query('name') name: Category) {
+	@ApiDocs.findDrinksByCategory('카테고리별 술 상세 정보 조회')
+	public async findDrinksByCategory(@Query('name') name: Category) {
 		if (!name) {
 			throw new BadRequestException();
 		}
-		return await this.drinksService.findByCategory(name);
+		return await this.drinksService.findDrinksByCategory(name);
 	}
 
 	@Get(':id')
-	public async findById(@Param('id') id: number) {
-		return await this.drinksService.findById(id);
+	@ApiDocs.findDrinkById('특정 술 상세 정보 조회')
+	public async findDrinkById(@Param('id') id: number) {
+		return await this.drinksService.findDrinkById(id);
+	}
+
+	@Get('/reviews/users/:id')
+	@UseGuards(JwtAuthGuard)
+	@ApiDocs.findUserReviewedDrinks('나의 술 저장고 - 리뷰한 술 목록')
+	public async findUserReviewedDrinks(@AuthUser() user: User) {
+		return await this.drinksService.findReviewedDrinksbyUser(user.id);
 	}
 }
