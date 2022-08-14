c7dbc298f658f33b8b14cbfe9b3828e755f32a01
sumin21 <tnals1178@naver.com>
2022-08-14 22:34:11 +0900

    Refactor: update users.seed.ts (seedUserData)

--- apps/api-server/src/database/seeds/users.seed.ts
+++ apps/api-server/src/database/seeds/users.seed.ts
@@ -1,39 +1,45 @@
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
 
+import { UsersProfile } from '@src/entities/users-profile.entity';
 import { DEFAULT_USER_PROFILE } from '@src/modules/users-profile/users-profile.constants';
 import { UserType } from '@src/types/users.types';
-import { UsersProfile } from '../../entities/users-profile.entity';
 import { User } from '../../entities/users.entity';
 
 const seedUserData = {
-	name: 'test martin',
-	nickname: 'test nickname',
-	email: 'testtest@email.com',
-	social_id: 123456789,
-	type: UserType.Kakao,
+	name: 'zuzu',
+	kakaoId: 2386981290,
+	email: 'zuzuu6271@gmail.com',
 };
 
 export class CreateInitialUserData implements Seeder {
 	public async run(factory: Factory, connection: Connection): Promise<any> {
-		const defaultUserProfile = DEFAULT_USER_PROFILE;
-
-		const usersProfile = await connection
-			.getRepository(UsersProfile)
-			.createQueryBuilder()
-			.select('users_profile')
-			.from(UsersProfile, 'users_profile')
-			.where('users_profile.image_url = :image_url', { image_url: defaultUserProfile })
-			.getOne();
-
 		const currentUser = await connection
 			.getRepository(User)
 			.createQueryBuilder()
 			.select('user')
 			.from(User, 'user')
-			.where('user.social_id = :social_id', { social_id: seedUserData.social_id })
+			.where('user.email = :email', { email: seedUserData.email })
 			.getOne();
 
-		if (!currentUser) await connection.getRepository(User).save({ ...seedUserData, profile: usersProfile });
+		if (!currentUser) {
+			const defaultUserProfile = DEFAULT_USER_PROFILE;
+			const usersProfile = await connection
+				.getRepository(UsersProfile)
+				.createQueryBuilder()
+				.select('users_profile')
+				.from(UsersProfile, 'users_profile')
+				.where('users_profile.image_url = :image_url', { image_url: defaultUserProfile })
+				.getOne();
+
+			await connection.getRepository(User).save({
+				name: seedUserData.name,
+				nickname: seedUserData.name,
+				email: seedUserData.email,
+				social_id: seedUserData.kakaoId,
+				type: UserType.Admin,
+				profile: usersProfile,
+			});
+		}
 	}
 }
