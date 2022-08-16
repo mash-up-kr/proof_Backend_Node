f4d171c514e0e429d61a8be198799786100c51bd
Gracechung-sw <hjngy0511@gmail.com>
2022-08-16 21:35:57 +0900

    Refactor: delete unused duplicate code

--- apps/api-server/src/modules/reviews/dto/review-item-response.dto.ts
+++ apps/api-server/src/modules/reviews/dto/review-item-response.dto.ts
@@ -7,15 +7,9 @@ export class ReviewItemResponseDto extends ReviewDto {
 
 	constructor(review) {
 		super(review);
-		this.id = review.id;
-		this.createdAt = review.createdAt;
-		this.mood = review.mood;
-		this.weather = review.weather;
-		this.time = review.time;
 		this.is_heavy = this.#decideThreshold(review.is_heavy) ? 'Light' : 'Heavy';
 		this.is_bitter = this.#decideThreshold(review.is_bitter) ? 'Sweet' : 'Bitter';
 		this.is_strong = this.#decideThreshold(review.is_strong) ? 'Mild' : 'Strong';
 		this.is_burning = this.#decideThreshold(review.is_burning) ? 'Smooth' : 'Burning';
-		this.taste = review.taste;
 	}
 }
