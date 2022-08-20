7002e9d097f5621e9bebe8d422426595f1bf5269
Gracechung-sw <hjngy0511@gmail.com>
2022-08-20 22:55:22 +0900

    fix

--- apps/api-server/src/modules/drinks/drinks.service.ts
+++ apps/api-server/src/modules/drinks/drinks.service.ts
@@ -6,7 +6,6 @@ import { Repository } from 'typeorm';
 import { Drink } from '@src/entities/drinks.entity';
 import { WorldcupResultItem } from '@src/entities/worldcup-result-item.entity';
 import { CreateDrinkDto } from './dto/create-drink.dto';
-import { WorldcupResultItem } from '@src/entities/worldcup-result-item.entity';
 import { DrinkCardResponseDto } from './dto/drink-card-response.dto';
 
 @Injectable()
